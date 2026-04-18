6252ad7915d4aaa7caf6f1598d4630f08fc07f15
sahilmahat <sahilmahat2@gmail.com>
2026-04-18 15:06:59 +0000

    update urls for production

--- backend/main.py
+++ backend/main.py
@@ -8,7 +8,11 @@ app=FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[
+        "http://localhost:5173"
+        "https://dockerfile-generator.vercel.app"
+        ],
+
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
